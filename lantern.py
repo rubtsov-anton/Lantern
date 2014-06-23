a7d8e510c0f366b8d4017b4701b7fd4bb216d7ff
Rubtsov Anton <rubtsov493@gmail.com>
2014-06-23 14:28:30 +0400

    Added some commentary

--- lantern.py
+++ lantern.py
@@ -13,7 +13,12 @@ class lantern:
         self.state = state
         self.color = color
         self.ip_port = (ip,port)
-
+        
+        """
+        Every new command should be added in this dict in format TYPE:FUNC
+        New commands should take exactly one argument, which is a byte string that should be struct.unpack'ed according to the meaning of input for that particular command
+        Each function that implements new command should end with self.redraw()
+        """
         self.commands = {
         0x12:self.on,
         0x13:self.off,
@@ -22,14 +27,12 @@ class lantern:
         self.socket = socket.socket()
         self.init_draw()
         
-    def connect(self):
-        try:
-            self.socket.connect(self.ip_port)
-        except:
-            print "Couldn't reach server"
-            self.closer(None,None)
-
-        
+    """
+    *draw commands are used for back-end drawing of lantern, they can be rewritten for fancier look
+    init_draw should initialize graphics if such initialization is needed and draw initial state of lantern
+    redraw is straightforward update of any changes in lantern's state
+    end_draw should correctly shut down graphical system
+    """
     def init_draw(self):
         if self.state:
             print "Lantern turned on and is shining with color %06X" % self.color
@@ -45,7 +48,9 @@ class lantern:
     def end_draw(self):
         pass
         
-        
+
+    "Commands that implement first version of lantern protocol"
+    
     def on(self,val):
         self.state = 1
         self.redraw()
@@ -59,8 +64,14 @@ class lantern:
         self.color = 0x010000*R + 0x000100*G+ B
         self.redraw()
         
+    
+    "Commands that implement networking"
         
     def tcp_client(self,fd,ev):
+        """ 
+        Callback function that should be provided as a handler for
+        READ and ERROR events on self.socket in tornado.ioloop
+        """
         io_loop = tornado.ioloop.IOLoop.current()
         if ev & tornado.ioloop.IOLoop.ERROR:
             self.socket.close()
@@ -87,11 +98,33 @@ class lantern:
                     self.closer(None,None)
         else:
             print "Unknown signal"
+
+    def connect(self):
+        """
+        Connect to server. Should be called after registering all handlers.
+        Does not invoke ioloop.start(). It should be invoked right after call to this function
+        """
+        try:
+            self.socket.connect(self.ip_port)
+        except:
+            print "Couldn't reach server"
+            self.closer(None,None)
         
     def closer(self,signum,frame):
+        """
+        Correctly closes everything that needs to be closed in lantern and stops tornado.ioloop.
+        Should be provided as a handler for SIGINT or any other signal that will be used as a stop signal for lantern
+        Also can be used as a "finally" statement for cleanup after emergency stop.
+        """
         print "Shutting down"
-        self.socket.close()
-        self.end_draw()
+        try:
+            self.socket.close()
+        except:
+            pass
+        try:
+            self.end_draw()
+        except:
+            pass
         io_loop = tornado.ioloop.IOLoop.current()
         io_loop.stop()
         sys.exit(0)
